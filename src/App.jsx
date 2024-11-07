0a2eef5323261b4d91b6f5674e8d8080fda47c15
tepposk <teppo.saarikoski@iki.fi>
2024-11-07 13:54:29 +0200

    second try

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './main.css';
+import Icon_email from './assets/email_icon.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-    
+      <h1 className="ns">Teppo Saarikoski</h1>
+      <p>
+        Hi there, welcome to my portfolio! The site is currently under construction but I'm hoping to get it up and running in November. Meanwhile you can <a _blank href="https://tepposk.github.io/portfolio/" target="_blank">check out my old portfolio</a> or connect with me on <a href="http://www.linkedin.com/in/tepposaarikoski" target="_blank">Linkedin</a>.
+      </p>
+      <p>- Teppo 😊</p>
+      <p><img src={Icon_email} className="contact-icon ns" /> teppo@tepposaarikoski.fi </p>
     </div>
   )
 }
